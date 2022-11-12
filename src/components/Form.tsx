e27fad89e961e5c9f5faf258cfb784b64d50d1b5
Shunsuke <shunsuke.takasu@gmail.com>
2022-11-12 20:54:12 +0900

    Edit:それぞれのEventHandlerに分ける + オブジェクトflagの追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,16 +4,27 @@ import {useDispatchTodos } from '../contexts/TodoContext';
 import {Input,Center,IconButton} from "@chakra-ui/react"
 import {AddIcon} from "@chakra-ui/icons"
 //追加画面
+
 const Form = () => {
   const dispatch = useDispatchTodos()
 
   const [inputVal, setInputVal] = useState("")
-  
-  const addTodo = (e) =>{
-    e.preventDefault()
+
+  const onClick = (event: React.MouseEvent<HTMLElement, MouseEvent>):void => {
+    event.preventDefault()
+    addTodo()
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault()
+    addTodo()
+  }
+
+  const addTodo = ():void =>{
     const newTodo = {
       id: Math.floor(Math.random()*1e5) ,
-      content: inputVal
+      content: inputVal,
+      flag: false
     }
     dispatch(
       {
@@ -27,9 +38,9 @@ const Form = () => {
   return (
     <div>
       <Center py="10" >
-        <form onSubmit ={addTodo}>
+        <form onSubmit ={onSubmit}>
           <Input width='auto' type="text" value={inputVal} onChange={(e)=>{setInputVal(e.target.value)}} />
-          <IconButton isRound="true" bg="blue.200" icon={<AddIcon/>} onClick={addTodo}/>
+          <IconButton aria-label="add Todo" isRound bg="blue.200" icon={<AddIcon/>} onClick={onClick} ></IconButton>
         </form>
       </Center>
       
